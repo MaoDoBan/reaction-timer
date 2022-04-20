9d28626bee51c99bd11b9236535e2264f61c3483
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-04-20 13:35:02 -0300

    Block.vue: add delay pra mostrar o componente

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock">
     click me
   </div>
 </template>
 
 <script>
 export default {
-  props: ["delay"]
+  props: ["delay"],
+  data(){ return {
+    showBlock: false
+  }; },
+  mounted(){
+    setTimeout(()=>{
+      this.showBlock = true;
+    }, this.delay);
+  },
+  updated(){
+    console.log("componente atualizado");
+  },
+  unmounted(){
+    console.log("desmontado");
+  }
 }
 </script>
 
