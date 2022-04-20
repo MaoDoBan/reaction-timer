0c6f0aa8700edca013a35f9d7049a40844184df9
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-04-20 17:18:28 -0300

    Results.vue: mostra o rank

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,30 @@
 <template>
-  <p>Reaction time: {{ this.score }} ms</p>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-  props: ["score"]
+  props: ["score"],
+  data(){ return {
+    rank: null
+  } ;},
+  mounted(){
+    if(this.score < 250){
+      this.rank = "Dedos ninjas";
+    }else if(this.score < 400){
+      this.rank = "Reflexos rápidos";
+    }else{
+      this.rank = "Lerdou...";
+    }
+  }
 }
 </script>
 
 <style>
-
+.rank{
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
 </style>
